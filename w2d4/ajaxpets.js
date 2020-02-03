71b21c6928f1cd3fa32b52fa57297915c99d519c
unknown <610488@CS.MUM.EDU>
2020-02-03 14:56:53 -0600

    ajax

---
+++ w2d4/ajaxpets.js
@@ -0,0 +1,41 @@
+/**
+ * JQuery Lab
+ */
+$(function(){
+
+    $("input[type='radio']").on('change',function(){
+        const petId = $(this)[0].id;
+        $.ajax({
+            'url': 'pet.json',
+            'method':'GET'
+        })
+        .done(function(data){
+            let resp = JSON.parse(data)
+            if(petId === 'kitties'){
+                displayPictures(resp.kitties)
+            }else{
+                displayPictures(resp.puppies)
+            }
+        })
+    })
+
+
+    /**
+     * .done handler to display images
+     * @param {Json} data 
+     */
+    function displayPictures(data) {
+        $("#pictures img").remove()
+        data.forEach(element => {
+            const $img = $('<img>',{
+                src:element.pet,
+                width:'200',
+                height:'200'
+            })
+            $("#pictures").append($img)
+        });
+        
+    }
+
+    
+})
